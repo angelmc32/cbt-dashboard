7b7bca4063601dba36b402a66d07985bf5f81981
Mel <angel.melendez.c@gmail.com>
2023-08-10 08:54:14 -0600

    Modify auth config for T3 env variables config

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -81,12 +81,10 @@ export const authOptions: (ctxReq: CtxOrReq) => NextAuthOptions = ({
             JSON.parse(credentials?.message ?? "{}") as Partial<SiweMessage>
           );
           const nonce = await getCsrfToken({ req: { headers: req?.headers } });
-          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL!);
+          const nextAuthUrl = new URL(env.NEXTAUTH_URL);
           // const nextAuthUrl = new URL(
           //   "https://" + process.env.NEXT_PUBLIC_VERCEL_URL!
           // );
-
-          console.log(nextAuthUrl);
 
           const result = await siwe.verify({
             signature: credentials?.signature ?? "",
